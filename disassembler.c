bf11c5591aca36650052ee0987e305d18612fbd1
Alex Richter <anprichter@gmail.com>
2023-12-30 12:26:53 -0700

    Hexdump

--- disassembler.c
+++ disassembler.c
@@ -2,14 +2,30 @@
 #include <stdint.h>
 
 int main(int argc, char **argv) {
-    uint8_t rom_bytes[512]; // TODO: Make this more precise
     FILE *rom_file = fopen("./pong.rom", "rb");
+    fseek(rom_file, 0, SEEK_END);
 
-    for (int i = 0; i <= 256; i++) {
+    unsigned int size = ftell(rom_file);
+    uint8_t rom_bytes[size];
+
+    rewind(rom_file);
+
+    for (int i = 0; i < size; i++) {
         fread(&rom_bytes[i], 1, 1, rom_file);
     }
 
-    for (int i = 0; i < 256/2; i+=2) {
-        printf("%02X%02X\n", rom_bytes[i], rom_bytes[i+1]);
+    fclose(rom_file);
+
+    for (int i = 0; i < size;) {
+        for (int j = 0; j < 8; j++) {
+            if (i >= size)
+                break;
+            printf("%02X%02X ", rom_bytes[i], rom_bytes[i+1]);
+            i += 2;
+        }
+        if (i >= size)
+            break;
+
+        printf("\n");
     }
 }
